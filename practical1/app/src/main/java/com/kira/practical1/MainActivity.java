e5b7eb26798d53c178a9fae909aeffc18f1bbe05
Your Name <you@example.com>
2020-01-03 12:50:20 +0530

    added messaging between apps

---
+++ practical1/app/src/main/java/com/kira/practical1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.kira.practical1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+    public static final String EXTRA_MESSAGE = "com.kira.practical1.extra.MESSAGE";
+    private EditText mMessageText;
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_reply);
+    }
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(secondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG,"clicked");
+        Intent intent = new Intent(this, secondActivity.class);
+        String message = mMessageText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+}
